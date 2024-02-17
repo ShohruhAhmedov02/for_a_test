d6c2af9585a3a755a529cf74922690abd6425888
ShohruhAhmedov02 <ahmedovshohruh0111@gmail.com>
2024-02-17 15:36:50 +0500

    my commit

--- maths2.py
+++ maths2.py
@@ -1,3 +1,7 @@
-a = int()
-b = int()
-a+b
+# range(start(optinal), end(required), step(optinal))
+
+lst=(1,2,4,5,8)
+
+for l in lst: #string, list, tuple, set, dictionary, range
+    print(l)
+    # lst o'zgaruvchilarno chiqar
